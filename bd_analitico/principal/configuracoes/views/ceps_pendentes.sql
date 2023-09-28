884c003dbb564d1f0b66105fdd8eb6187a3e9c73
maira <maaottoni@gmail.com>
2023-09-28 10:29:45 -0300

    Adiciona códigos das viewa  do schema configuracoes do banco principal

---
+++ bd_analitico/principal/configuracoes/views/ceps_pendentes.sql
@@ -0,0 +1,7 @@
+-- configuracoes.ceps_pendentes source
+
+CREATE OR REPLACE VIEW configuracoes.ceps_pendentes
+AS SELECT DISTINCT aih.usuario_residencia_cep AS id_cep
+   FROM dados_publicos._sihsus_aih_reduzida_disseminacao aih
+     LEFT JOIN listas_de_codigos.ceps cep ON aih.usuario_residencia_cep = cep.id_cep
+  WHERE cep.id_cep IS NULL;
