d3da66817ee740680da8225f878bba226b1b843e
Maira <maaottoni@gmail.com>
2024-03-20 19:49:44 -0300

    Corrige funções de sincronização

--- bd_analitico/esus-backups/impulso_previne_dados_nominais/funcoes/sincronizar_dados_nominais_producao.sql
+++ bd_analitico/esus-backups/impulso_previne_dados_nominais/funcoes/sincronizar_dados_nominais_producao.sql
@@ -7,11 +7,11 @@ begin
     FOR tabela IN 
         SELECT table_name FROM information_schema.tables 
         WHERE table_schema = '_impulso_previne_dados_nominais_bd_producao'
-        and table_name in ('painel_cadastros_gestantes_duplicadas'
-							'painel_citopatologico_lista_nominal'
-							'painel_enfermeiras_lista_nominal_diabeticos'
-							'painel_enfermeiras_lista_nominal_hipertensos'
-							'painel_gestantes_lista_nominal'
+        and table_name in ('painel_cadastros_gestantes_duplicadas',
+							'painel_citopatologico_lista_nominal',
+							'painel_enfermeiras_lista_nominal_diabeticos',
+							'painel_enfermeiras_lista_nominal_hipertensos',
+							'painel_gestantes_lista_nominal',
 							'painel_vacinacao_lista_nominal')        
     LOOP
 	    SET ROLE postgres;
