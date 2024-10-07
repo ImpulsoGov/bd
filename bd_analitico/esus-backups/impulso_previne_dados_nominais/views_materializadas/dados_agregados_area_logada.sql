bdc905c56a7fb91cd85ec5c396ce141aa4d6b7c3
Maira <maaottoni@gmail.com>
2024-10-07 17:33:43 -0300

    aplica correções

--- bd_analitico/esus-backups/impulso_previne_dados_nominais/views_materializadas/dados_agregados_area_logada.sql
+++ bd_analitico/esus-backups/impulso_previne_dados_nominais/views_materializadas/dados_agregados_area_logada.sql
@@ -1,136 +1,282 @@
-CREATE MATERIALIZED VIEW impulso_previne_dados_nominais.dados_agregados_area_logada
-TABLESPACE pg_default
-as WITH diabeticos AS (
-    SELECT 
-        tbd.municipio_id_sus,
-        tbd.equipe_ine_cadastro AS equipe_ine,
-        'DIABETES' AS indicador,
-        'SOLICITAR_HEMOGLOBINA' AS parametro_descricao,
-        COUNT(*) AS parametro_total,
-        SUM(CASE WHEN tbd.status_usuario = 'Consulta e solicitação de hemoglobina a fazer' THEN 1 ELSE 0 END) AS parametro_valor,
-        CURRENT_TIMESTAMP AS atualizacao_data 
-    FROM impulso_previne_dados_nominais_replica.painel_enfermeiras_lista_nominal_diabeticos tbd
-    GROUP BY tbd.municipio_id_sus, tbd.equipe_ine_cadastro
-),
-hipertensos AS (
-    SELECT 
-        tbh.municipio_id_sus,
-        tbh.equipe_ine_cadastro AS equipe_ine,
-        'HIPERTENSOS' AS indicador,
-        'AFERIR_PA' AS parametro_descricao,
-        COUNT(*) AS parametro_total,
-        SUM(CASE WHEN tbh.status_usuario = 'Consulta e aferição de PA a fazer' THEN 1 ELSE 0 END) AS parametro_valor,
-        CURRENT_TIMESTAMP AS atualizacao_data 
-    FROM impulso_previne_dados_nominais_replica.painel_enfermeiras_lista_nominal_hipertensos tbh
-    GROUP BY tbh.municipio_id_sus, tbh.equipe_ine_cadastro
-),
-citopatologico AS (
-    SELECT 
-        tbc.municipio_id_sus,
-        tbc.equipe_ine,
-        'CITOPATOLOGICO' AS indicador,
-        'COLETAR_EXAME_CITO' AS parametro_descricao,
-        COUNT(*) AS parametro_total,
-        SUM(CASE WHEN tbc.id_status_usuario <> 12 THEN 1 ELSE 0 END) AS parametro_valor, -- Quando status diferente de 12 (Em Dia)
-        CURRENT_TIMESTAMP AS atualizacao_data 
-    FROM impulso_previne_dados_nominais_replica.painel_citopatologico_lista_nominal tbc
-    GROUP BY tbc.municipio_id_sus, tbc.equipe_ine
-),
-vacinacao AS (
-    SELECT 
-        tbv.municipio_id_sus,
-        tbv.equipe_ine,
-        'VACINACAO' AS indicador,
-        'DOSE_ATRASADA' AS parametro_descricao,
-        COUNT(*) AS parametro_total,
-        SUM(CASE WHEN tbv.id_status_polio = 3 OR tbv.id_status_penta = 3 THEN 1 ELSE 0 END) AS parametro_valor, -- Quando status polio ou penta em atraso
-        CURRENT_TIMESTAMP AS atualizacao_data 
-    FROM impulso_previne_dados_nominais_replica.painel_vacinacao_lista_nominal tbv
-    WHERE tbv.id_status_quadrimestre = 1 -- Quadrimestre atual
-    GROUP BY tbv.municipio_id_sus, tbv.equipe_ine
-),
-gestantes_6_consultas AS (
-    SELECT 
-        tb6.municipio_id_sus,
-        tb6.equipe_ine,
-        'PRE_NATAL' AS indicador,
-        'MENOS_6_CONSULTAS' AS parametro_descricao,
-        COUNT(*) AS parametro_total,
-        SUM(CASE 
-                WHEN tb6.gestacao_idade_gestacional_primeiro_atendimento >= 0 
-                AND tb6.gestacao_idade_gestacional_primeiro_atendimento <= 12 
-                AND tb6.consultas_pre_natal_validas < 6 -- gestantes com menos de 6 consultas em 12 semanas
-                THEN 1 
-                ELSE 0 
-            END) AS parametro_valor,
-        CURRENT_TIMESTAMP AS atualizacao_data 
-    FROM impulso_previne_dados_nominais_replica.painel_gestantes_lista_nominal tb6
-    WHERE tb6.gestacao_quadrimestre = ( -- gestação quadrimestre = quadri atual OBS! não inclui gestantes sem dum
-        CASE 
-            WHEN DATE_PART('month', CURRENT_DATE) >= 1 AND DATE_PART('month', CURRENT_DATE) <= 4 THEN CONCAT(DATE_PART('year', CURRENT_DATE), '.Q1')
-            WHEN DATE_PART('month', CURRENT_DATE) >= 5 AND DATE_PART('month', CURRENT_DATE) <= 8 THEN CONCAT(DATE_PART('year', CURRENT_DATE), '.Q2')
-            WHEN DATE_PART('month', CURRENT_DATE) >= 9 AND DATE_PART('month', CURRENT_DATE) <= 12 THEN CONCAT(DATE_PART('year', CURRENT_DATE), '.Q3')
-        END
-    )                        
-    GROUP BY tb6.municipio_id_sus, tb6.equipe_ine
-),
-gestantes_odonto_indetificado AS (
-    SELECT 
-        tbo.municipio_id_sus,
-        tbo.equipe_ine,
-        'PRE_NATAL' AS indicador,
-        'ODONTO' AS parametro_descricao,
-        COUNT(*) AS parametro_total,
-        SUM(CASE 
-                WHEN tbo.id_atendimento_odontologico <> 1 -- Atend. odontológico NÃO identificado
-                THEN 1 
-                ELSE 0 
-            END) AS parametro_valor,
-        CURRENT_TIMESTAMP AS atualizacao_data 
-    FROM impulso_previne_dados_nominais_replica.painel_gestantes_lista_nominal tbo
-    WHERE tbo.gestacao_quadrimestre = (
-        CASE 
-            WHEN DATE_PART('month', CURRENT_DATE) >= 1 AND DATE_PART('month', CURRENT_DATE) <= 4 THEN CONCAT(DATE_PART('year', CURRENT_DATE), '.Q1')
-            WHEN DATE_PART('month', CURRENT_DATE) >= 5 AND DATE_PART('month', CURRENT_DATE) <= 8 THEN CONCAT(DATE_PART('year', CURRENT_DATE), '.Q2')
-            WHEN DATE_PART('month', CURRENT_DATE) >= 9 AND DATE_PART('month', CURRENT_DATE) <= 12 THEN CONCAT(DATE_PART('year', CURRENT_DATE), '.Q3')
-        END
-    )                        
-    GROUP BY tbo.municipio_id_sus, tbo.equipe_ine
-),
-gestantes_sifilis_hiv AS (
-    SELECT 
-        tbe.municipio_id_sus,
-        tbe.equipe_ine,
-        'PRE_NATAL' AS indicador,
-        'SIFILIS_HIV' AS parametro_descricao,
-        COUNT(*) AS parametro_total,
-        SUM(CASE 
-                WHEN tbe.id_exame_hiv_sifilis <> 4 -- Todos os demais status que não sejam os dois exames realizados
-                THEN 1 
-                ELSE 0 
-            END) AS parametro_valor,
-        CURRENT_TIMESTAMP AS atualizacao_data 
-    FROM impulso_previne_dados_nominais_replica.painel_gestantes_lista_nominal tbe
-    WHERE tbe.gestacao_quadrimestre = (
-        CASE 
-            WHEN DATE_PART('month', CURRENT_DATE) >= 1 AND DATE_PART('month', CURRENT_DATE) <= 4 THEN CONCAT(DATE_PART('year', CURRENT_DATE), '.Q1')
-            WHEN DATE_PART('month', CURRENT_DATE) >= 5 AND DATE_PART('month', CURRENT_DATE) <= 8 THEN CONCAT(DATE_PART('year', CURRENT_DATE), '.Q2')
-            WHEN DATE_PART('month', CURRENT_DATE) >= 9 AND DATE_PART('month', CURRENT_DATE) <= 12 THEN CONCAT(DATE_PART('year', CURRENT_DATE), '.Q3')
-        END
-    )                        
-    GROUP BY tbe.municipio_id_sus, tbe.equipe_ine
+WITH diabeticos AS (
+         WITH base AS (
+		    SELECT 
+		        tbd.municipio_id_sus,
+		        tbd.equipe_ine_cadastro AS equipe_ine,
+		        'DIABETES'::text AS indicador,
+		        COUNT(DISTINCT cidadao_nome || dt_nascimento) AS total_valor,
+		        COUNT(
+		            DISTINCT CASE
+		                WHEN tbd.status_usuario = 'Consulta e solicitação de hemoglobina a fazer'::text 
+		                THEN cidadao_nome || dt_nascimento
+		                ELSE NULL
+		            END
+		        ) AS fora_do_indicador_valor,
+		        current_time AS atualizacao_data
+		    FROM impulso_previne_dados_nominais_replica.painel_enfermeiras_lista_nominal_diabeticos tbd
+		    GROUP BY tbd.municipio_id_sus, tbd.equipe_ine_cadastro
+	)
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'FORA_DO_INDICADOR'::text AS parametro_descricao,
+	    fora_do_indicador_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+	UNION ALL
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'TOTAL'::text AS parametro_descricao,
+	    total_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
 )
-SELECT * FROM gestantes_6_consultas
+, hipertensos AS (
+		WITH base AS (
+		    SELECT 
+		        tbd.municipio_id_sus,
+		        tbd.equipe_ine_cadastro AS equipe_ine,
+		        'HIPERTENSOS'::text AS indicador,
+		        COUNT(DISTINCT cidadao_nome || dt_nascimento) AS total_valor,
+		        COUNT(
+		            DISTINCT CASE
+		                WHEN tbd.status_usuario = 'Consulta e aferição de PA a fazer'::text 
+		                THEN cidadao_nome || dt_nascimento
+		                ELSE NULL
+		            END
+		        ) AS fora_do_indicador_valor,
+		        current_time AS atualizacao_data
+		    FROM impulso_previne_dados_nominais_replica.painel_enfermeiras_lista_nominal_hipertensos tbd
+		    GROUP BY tbd.municipio_id_sus, tbd.equipe_ine_cadastro
+	)
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'FORA_DO_INDICADOR'::text AS parametro_descricao,
+	    fora_do_indicador_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+	UNION ALL
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'TOTAL'::text AS parametro_descricao,
+	    total_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+), 
+citopatologico AS (
+         WITH base AS (
+		    SELECT 
+		        tbc.municipio_id_sus,
+		        tbc.equipe_ine AS equipe_ine,
+		        'CITOPATOLOGICO'::text AS indicador,
+		        COUNT(DISTINCT tbc.paciente_nome||tbc.cidadao_cpf_dt_nascimento) AS total_valor,
+		        COUNT(
+		            DISTINCT CASE
+		                WHEN  tbc.id_status_usuario <> 12 -- Quando status diferente de 12 (Em Dia)
+		                THEN tbc.paciente_nome||tbc.cidadao_cpf_dt_nascimento
+		                ELSE NULL
+		            END
+		        ) AS fora_do_indicador_valor,
+		        current_time AS atualizacao_data
+		    FROM impulso_previne_dados_nominais_replica.painel_citopatologico_lista_nominal tbc
+		    GROUP BY tbc.municipio_id_sus, tbc.equipe_ine
+	)
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'FORA_DO_INDICADOR'::text AS parametro_descricao,
+	    fora_do_indicador_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+	UNION ALL
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'TOTAL'::text AS parametro_descricao,
+	    total_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+), 
+vacinacao AS (
+         WITH base AS (
+		    SELECT 
+		        tbv.municipio_id_sus,
+		        tbv.equipe_ine AS equipe_ine,
+		        'VACINACAO'::text AS indicador,
+		        COUNT(DISTINCT tbv.cidadao_nome ||tbv.cidadao_cpf_dt_nascimento) AS total_valor,
+		        COUNT(
+		            DISTINCT 
+		                CASE
+		                    WHEN tbv.id_status_polio = 3 OR tbv.id_status_penta = 3 -- Quando status polio ou penta em atraso
+		                THEN tbv.cidadao_nome||tbv.cidadao_cpf_dt_nascimento
+		                ELSE NULL
+		            END
+		        ) AS fora_do_indicador_valor,
+		        current_time AS atualizacao_data
+		    FROM impulso_previne_dados_nominais_replica.painel_vacinacao_lista_nominal tbv
+		    WHERE tbv.id_status_quadrimestre = 1
+		    GROUP BY tbv.municipio_id_sus, tbv.equipe_ine
+	)
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'FORA_DO_INDICADOR'::text AS parametro_descricao,
+	    fora_do_indicador_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+	UNION ALL
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'TOTAL'::text AS parametro_descricao,
+	    total_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+), 
+gestantes_6_consultas AS (
+         WITH base AS (
+		    SELECT 
+		         tb6.municipio_id_sus,
+		         tb6.equipe_ine,
+		        'PRE_NATAL_6_CONSULTAS'::text AS indicador,
+		        COUNT(DISTINCT tb6.chave_id_gestacao) AS total_valor,
+		        COUNT(
+		            DISTINCT 
+		                CASE
+		                    WHEN tb6.gestacao_idade_gestacional_primeiro_atendimento >= 0 
+		                    AND tb6.gestacao_idade_gestacional_primeiro_atendimento <= 12 
+		                    AND tb6.consultas_pre_natal_validas < 6  -- gestantes com menos de 6 consultas em 12 semanas
+		                THEN tb6.chave_id_gestacao 
+		                ELSE NULL
+		            END
+		        ) AS fora_do_indicador_valor,
+		        current_time AS atualizacao_data
+		    FROM impulso_previne_dados_nominais_replica.painel_gestantes_lista_nominal tb6
+		    WHERE tb6.gestacao_data_dpp::DATE > CURRENT_DATE -- gestantes ativas
+		    GROUP BY tb6.municipio_id_sus, tb6.equipe_ine
+	)
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'FORA_DO_INDICADOR'::text AS parametro_descricao,
+	    fora_do_indicador_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+	UNION ALL
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'TOTAL'::text AS parametro_descricao,
+	    total_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+), 
+gestantes_odonto_indetificado AS (
+         WITH base AS (
+		    SELECT 
+		         tbo.municipio_id_sus,
+		         tbo.equipe_ine,
+		        'PRE_NATAL_ODONTO'::text AS indicador,
+		        COUNT(DISTINCT tbo.chave_id_gestacao) AS total_valor,
+		        COUNT(
+		            DISTINCT 
+		                CASE
+		                    WHEN tbo.id_atendimento_odontologico <> 1 -- Atend. odontológico NÃO identificado
+		                THEN tbo.chave_id_gestacao 
+		                ELSE NULL
+		            END
+		        ) AS fora_do_indicador_valor,
+		        current_time AS atualizacao_data
+		    FROM impulso_previne_dados_nominais_replica.painel_gestantes_lista_nominal tbo
+		    WHERE tbo.gestacao_data_dpp::DATE > CURRENT_DATE -- gestantes ativas
+		    GROUP BY tbo.municipio_id_sus, tbo.equipe_ine
+	)
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'FORA_DO_INDICADOR'::text AS parametro_descricao,
+	    fora_do_indicador_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+	UNION ALL
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'TOTAL'::text AS parametro_descricao,
+	    total_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+), 
+gestantes_sifilis_hiv AS (
+         WITH base AS (
+		    SELECT 
+		         tbe.municipio_id_sus,
+		         tbe.equipe_ine,
+		        'PRE_NATAL_SIFILIS_HIV'::text AS indicador,
+		        COUNT(DISTINCT tbe.chave_id_gestacao) AS total_valor,
+		        COUNT(
+		            DISTINCT 
+		                CASE
+		                    WHEN tbe.id_exame_hiv_sifilis <> 4 -- Todos os demais status que não sejam os dois exames realizados
+		                THEN tbe.chave_id_gestacao 
+		                ELSE NULL
+		            END
+		        ) AS fora_do_indicador_valor,
+		        current_time AS atualizacao_data
+		    FROM impulso_previne_dados_nominais_replica.painel_gestantes_lista_nominal tbe
+		    WHERE tbe.gestacao_data_dpp::DATE > CURRENT_DATE -- gestantes ativas
+		    GROUP BY tbe.municipio_id_sus, tbe.equipe_ine
+	)
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'FORA_DO_INDICADOR'::text AS parametro_descricao,
+	    fora_do_indicador_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+	UNION ALL
+	SELECT 
+	    municipio_id_sus,
+	    equipe_ine,
+	    indicador,
+	    'TOTAL'::text AS parametro_descricao,
+	    total_valor AS parametro_valor,
+	    atualizacao_data
+	FROM base
+)
+ SELECT *
+   FROM gestantes_6_consultas
 UNION ALL
-SELECT * FROM gestantes_odonto_indetificado
+ SELECT *
+   FROM gestantes_odonto_indetificado
 UNION ALL
-SELECT * FROM gestantes_sifilis_hiv
+ SELECT *
+   FROM gestantes_sifilis_hiv
 UNION ALL
-SELECT * FROM diabeticos
+ SELECT *
+   FROM diabeticos
 UNION ALL
-SELECT * FROM hipertensos
+ SELECT *
+   FROM hipertensos
 UNION ALL
-SELECT * FROM citopatologico
+ SELECT *
+   FROM citopatologico
 UNION ALL
-SELECT * FROM vacinacao
-WITH DATA;
+ SELECT *
+   FROM vacinacao
